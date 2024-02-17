fb6856da0f2fc6cec33efe140df219b897161677
SabrinaHirani <sabrinahirani@gmail.com>
2024-02-17 02:31:51 -0500

    D3.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import { Heading } from '@chakra-ui/react';
+import D3Graph from './D3Graph';
+import './styles.css';
+
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <Heading>Name</Heading>
+      <D3Graph />
     </div>
   );
 }
---
+++ client/src/D3Graph.js
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
+
+const D3Graph = () => {
+  const ref = useRef();
+
+  useEffect(() => {
+    // D3.js code to create the graph
+    const svg = d3.select(ref.current)
+      .append("svg")
+      .attr("width", 400)
+      .attr("height", 400)
+      .style("border", "1px solid black");
+
+    svg.append("circle")
+      .attr("cx", 200)
+      .attr("cy", 200)
+      .attr("r", 100)
+      .attr("fill", "red");
+  }, []);
+
+  return (
+    <div ref={ref}></div>
+  );
+};
+
+export default D3Graph;
